c38bf8bf55c1ab61d354e47e5ad40d6384cb9896
Rafael Moreira <rafamsilva@gmail.com>
2025-04-08 11:20:03 +1000

    .dev-dependencies-update Dependecy Update (Removal) - Updated AppModule to use DSL from Koin - Minor Update on ApplicationClass

--- app/src/main/java/com/exercise/matipv2/di/AppModule.kt
+++ app/src/main/java/com/exercise/matipv2/di/AppModule.kt
@@ -3,12 +3,12 @@ package com.exercise.matipv2.di
 import com.exercise.matipv2.data.repository.MatipRepository
 import com.exercise.matipv2.data.repository.OfflineMatipRepository
 import com.exercise.matipv2.ui.MainScreenViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
-    single<MatipRepository> {
-        OfflineMatipRepository(tipDao = get(), listDao = get())
-    }
-    viewModel() { MainScreenViewModel(get()) }
+    singleOf(::OfflineMatipRepository) { bind<MatipRepository>() }
+    viewModelOf(::MainScreenViewModel)
 }
