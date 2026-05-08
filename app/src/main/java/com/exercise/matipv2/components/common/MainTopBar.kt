a60a8f13e0935c39e39a236cd4900de5dabf0953
Rafael Moreira <rafamsilva@gmail.com>
2026-05-08 11:56:56 +1000

    .develop Replacement to topAppBarColors

--- app/src/main/java/com/exercise/matipv2/components/common/MainTopBar.kt
+++ app/src/main/java/com/exercise/matipv2/components/common/MainTopBar.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -26,8 +27,12 @@ import com.exercise.matipv2.R
 fun MainTopBar() {
     CenterAlignedTopAppBar(
         modifier = Modifier.semantics { contentDescription = "Top Bar" },
-        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+        colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.surfaceTint.copy(alpha = 0.1f),
+            scrolledContainerColor = Color.Unspecified,
+            navigationIconContentColor = Color.Unspecified,
+            titleContentColor = Color.Unspecified,
+            actionIconContentColor = Color.Unspecified
         ),
         title = {
             Image(
